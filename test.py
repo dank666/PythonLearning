1266aa71e6a54a9a8281f18b0b847a6b5eb8286f
wtejing <wtejing@gmail.com>
2024-11-01 17:12:02 +0800

    test

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+PythonLearning -
+Author: TeJing Wang
+Date: 2024/11/1
+"""
+
+print("42312205 王特警")
+print('------------------------')
+print("hope things become good.")
