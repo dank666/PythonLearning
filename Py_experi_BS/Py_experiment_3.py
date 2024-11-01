8da046201a7591372fdd7250484852a1cd0415d4
wtejing <wtejing@gmail.com>
2024-11-01 19:59:56 +0800

    My undergraduate Python assignment

--- Py_experi_BS/Py_experiment_3.py
+++ Py_experi_BS/Py_experiment_3.py
@@ -8,3 +8,14 @@ Date: 2024/11/1
 """
 
 print('------------------------')
+import wordcloud
+import re
+import matplotlib.pyplot as plt
+import jieba
+from urllib.request import urlopen
+
+startUrl = r'http://ccs.snnu.edu.cn/xygk/lsyg1.htm'
+with urlopen(startUrl) as fp:
+    content = fp.read().decode()
+
+#提取并遍历每个事件链接
